5d7d8578083fd6aa31cccbe7d1a36b58b3924d97
MrRainx <diegocondo10@yahoo.es>
2020-01-06 21:19:04 -0500

    agregue los archivos estaticos y tambien estoy explicando como trabaja react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(10)
+
+  console.log('HOLA MUNDO')
+
+  function incrementar(e) {
+    setCounter(counter + 1)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+
+      <div className="text-center" style={{ marginTop: '10%', }}>
+
+        <h1 className="text-center">{counter}</h1>
+
+        <button className="btn btn-info"
+          onClick={e => incrementar(e)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Incremenetar
+        </button>
+
+
+      </div>
+
+
+    </>
   );
 }
 
